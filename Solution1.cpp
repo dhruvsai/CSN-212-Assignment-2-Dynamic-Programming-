f5122d2562822546739f97c274b77e3d537bb64f
dhruvsai <dhruvsai1151@gmail.com>
2017-03-04 14:41:27 +0530

    Update Solution1.cpp

--- Solution1.cpp
+++ Solution1.cpp
@@ -1,52 +1,24 @@
 // Author: Dhruv Pachauri,13117022
 
 #include <bits/stdc++.h>
-
-#define SIZE 50
-#define debug(x) {cout << #x << " : " << x << endl ;}
 using namespace std;
 
-int main(){
-    
-    int a[SIZE] = {396, 549, 22, 819, 611, 972, 730, 638, 978, 342, 566, 514, 752, 
-                   871, 911, 172, 488, 542, 482, 974, 210, 474, 66, 387, 1, 872, 799, 
-                   262, 567, 113, 578, 308, 813, 515, 716, 905, 434, 101, 632, 450, 
-                   74, 254, 1000, 780, 633, 496, 513, 772, 925, 746};
+vector<int> a;
 
+int FindLongestZigZag(){
+	int SIZE = a.size();
+	
     int signs[SIZE] = {0};
     int dp[SIZE] = {1};
     // we have to find the longest zig zag sequence till i
     for(int i=1;i<SIZE;i++){
-        /* 
-        * For each i, we should figure out the previous element to jump from.
-        * it can be the one right before i or it could be element at index 0.
-        * so we have to go all the way from i-1 down to 0.
-        */
-        
         for(int j = i-1; j>=0; j--){
-            /* 
-            * What's most important is.. we cannot jump from any element.. 
-            * we are looking for an element which when subtracted from this 
-            * has the desired sign. variable sign indicates whether the previous 
-            * was a positive jump or a negative jump. 
-            */
             
             int sign = signs[j];
             
             if( (sign == 0) || ( (sign < 0) && (a[i] - a[j] > 0) ) || 
                     ( (sign > 0) && (a[i] - a[j] < 0) ) ){
-                 /*
-                 The different cases.
-                 1. sign == 0 : We are just beginning.. we can pick either 
-                        positive difference or negative difference.
-                        
-                 2. (sign < 0) && (a[i] - a[j] > 0) : Previous value resulted in a negative 
-                        difference, so we need to pick a positive differencevalue
-                 3. (sign > 0) && (a[i] - a[j] < 0) : Previous value resulted in a positive 
-                        difference, pick negative difference this time.
-                 Once we have found an element, store the difference in signs, so that it 
-                 helps in subsequent iterations.
-                */
+                
                 dp[i] = dp[j] + 1;
                 
                 signs[i] = a[i] - a[j];
@@ -55,6 +27,19 @@ int main(){
         }
     }
 
-    cout << dp[SIZE-1] << endl;
+    return dp[SIZE-1];
+}
+int main(){
+    
+    a.clear();
+	a = { 374, 40, 854, 203, 203, 156, 362, 279, 812, 955, 
+				600, 947, 978, 46, 100, 953, 670, 862, 568, 188, 
+				67, 669, 810, 704, 52, 861, 49, 640, 370, 908, 
+				477, 245, 413, 109, 659, 401, 483, 308, 609, 120, 
+				249, 22, 176, 279, 23, 22, 617, 462, 459, 244
+				};
+				
+	cout<<FindLongestZigZag()<<endl;
+
     return 0;
 }
